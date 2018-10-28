c888946522f9faef0875ab47bcb10a1528dcb27e
GabrielaSO <gaitsotoor@ittepic.edu.mx>
2018-10-28 01:45:50 -0700

    configura credenciales

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyCeu4Sxtc5zOs3IgSiw1TkYzmwi75qlIBI",
+    authDomain: "dam20182-orozco-u2-t07.firebaseapp.com",
+    databaseURL: "https://dam20182-orozco-u2-t07.firebaseio.com",
+    projectId: "dam20182-orozco-u2-t07",
+    storageBucket: "dam20182-orozco-u2-t07.appspot.com",
+    messagingSenderId: "353637383623"
+    }
+  };
