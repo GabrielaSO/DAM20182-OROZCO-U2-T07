53a3c35f2c6a3089cfca3deff4a7e1a255a489d7
GabrielaSO <gaitsotoor@ittepic.edu.mx>
2018-10-28 02:51:52 -0700

    declara modulos y cambia pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,65 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { MenuPage } from '../pages/menu/menu';
+import { DetailsPage } from '../pages/details/details';
+import { NewTaskModalPage } from '../pages/new-task-modal/new-task-modal';
+
+import { ImagePicker } from '@ionic-native/image-picker';
+import { FirebaseService } from '../pages/services/firebase.service';
+import { AuthService } from '../pages/services/auth.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
+/*import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+*/
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+//    HomePage,
+    LoginPage,
+    RegisterPage,
+    MenuPage,
+    NewTaskModalPage,
+    DetailsPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,  
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+//    HomePage,
+    LoginPage,
+    RegisterPage,
+    MenuPage,
+    NewTaskModalPage,
+    DetailsPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseService,
+    AuthService,
+    ImagePicker,
   ]
 })
 export class AppModule {}
